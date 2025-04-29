fb3585dbd459faa13436d73e4ba503af07cb1a4a
codeByVladimir <v_zadorko@mail.ru>
2025-04-29 17:35:18 +0300

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const API_KEY = 'be7190c1';
+const popularMovies = [
+    'Die Hard', 
+    'The last of Us', 
+    'predator',
+    'wednesday',
+    'squid game',
+    'terminator',
+]
+const popularAnime = [
+    'jojo',
+    'naruto',
+    'my hero academia',
+    'kimetsu no yaiba',
+    'your name',
+    'hunter x hunter'
+]
