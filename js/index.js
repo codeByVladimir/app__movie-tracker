05479f8c0eed94467df4638d7c8b457188e73d14
codeByVladimir <v_zadorko@mail.ru>
2025-05-01 20:17:06 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,68 @@
+const movieslistNode = document.querySelector('.main__list-movies');
+const animelistNode = document.querySelector('.main__list-anime');
+
+const URL = 'http://www.omdbapi.com/?apikey=be7190c1&t=';
+
+const popularMovies = [
+    'Joker', 
+    'the last of us', 
+    'the gray man',
+    'wednesday',
+    'squid game',
+    'Now You See Me',
+];
+
+const popularAnime = [
+    "JoJo's Bizarre Adventure",
+    'Naruto: Shippuden',
+    'my hero academia',
+    'kimetsu no yaiba',
+    'Death Note',
+    'hunter x hunter'
+];
+
+
+
+async function parseInfo(url, movies){
+    try{
+        const parseData = [];
+        for (const element of movies) {
+            const getInfo = await fetch(`${url}${encodeURIComponent(element)}`);
+            const response = await getInfo.json();
+            parseData.push(response)
+        }
+        return parseData;
+        
+    }catch(error){
+        console.log(error);
+    }
+}
+
+function htmlCreateElement(object, listNode){
+    for (const element of object) {
+        listNode.innerHTML += `
+        <li class="main__content-item" data-id="${element.Title}">
+            <div class="main__content-img-wrapper">
+                <img class="main__content-image" src="${element.Poster}" alt="${element.Title}">
+            </div>
+        </li>`
+    }
+}
+
+movieslistNode.addEventListener('click', (event) => {
+    const getDataId = event.target.closest('.main__content-item');
+    let dataId = '';
+    if(getDataId){
+        dataId = getDataId.dataset.id;
+    }
+    window.location.href = `movie.html?id=${dataId}`;
+})
+
+async function start(){
+    const movies = await parseInfo(URL, popularMovies);
+    const anime = await parseInfo(URL, popularAnime)
+    htmlCreateElement(movies, movieslistNode);
+    htmlCreateElement(anime, animelistNode)
+}
+
+start();
---
+++ js/movie.js
@@ -0,0 +1,6 @@
+const titleNode = document.querySelector('.main__title');
+const posterNode = document.querySelector('.main__poster');
+const listNode = document.querySelector('.main__role-list');
+const descriptionNode = document.querySelector('.main__description');
+const ratingNode = document.querySelector('.main__rating');
+
