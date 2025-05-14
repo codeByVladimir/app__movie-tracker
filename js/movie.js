2a6ab15bc47c202061fdafac9ed32d6adf6901a4
codeByVladimir <v_zadorko@mail.ru>
2025-05-14 05:47:12 +0300

    Add files via upload

--- js/movie.js
+++ js/movie.js
@@ -3,9 +3,10 @@ const posterNode = document.querySelector('.main__poster');
 const listNode = document.querySelector('.main__role-list');
 const descriptionNode = document.querySelector('.main__description');
 const ratingNode = document.querySelector('.main__rating');
-const trailerNode = document.querySelector('.main__video');
+const trailerNode = document.querySelector('.main__video-trailer');
 
 const URL = 'http://www.omdbapi.com/?apikey=be7190c1&i=';
+const API_KEY = 'AIzaSyAx8Tg_iKsb_NUSsz_D8ACtz6Pq4OstJJE';
 
 async function getInfo(){
     const params = Object.fromEntries(new URLSearchParams(window.location.search));
@@ -13,19 +14,19 @@ async function getInfo(){
     const response = await request.json();
     return response;
 }
-async function parseTrailer(data) {
-    const urlParseVideo = 'https://yandex.ru/video/search?text=';
-    const encodedURL = encodeURIComponent(`${urlParseVideo}трейлер ${data.Title}`);
-    const request = await fetch(`https://api.allorigins.win/get?url=${encodedURL}`);
-    const response = await request.json();
-    const parserDOM = new DOMParser();
-    const doc = parserDOM.parseFromString(response, 'text/html');
-    console.log(doc);
+
+async function parseTrailer(data, apikey) {
+    const query = `trailer ${data.Title}`
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${apikey}&type=video&part=snippet&q=${encodeURIComponent(query)}`;
+    
+    const request = await fetch(url);
+    const response = await request.json()
+    return `https://www.youtube.com/embed/${response.items[0].id.videoId}`;
 }
 
 
 
-function createContent(data) {
+function createContent(data, videoId) {
     titleNode.textContent = data.Title
     posterNode.src = data.Poster;
     posterNode.alt = data.Title;
@@ -35,13 +36,16 @@ function createContent(data) {
     }
     descriptionNode.textContent = data.Plot;
     ratingNode.textContent = `IMDB Rating ⭐${data.imdbRating}`;
+    trailerNode.innerHTML += `
+    <iframe class="main__trailer" width="521px" height="291px" src="${videoId}" frameborder="0" allowfullscreen></iframe>
+    `
     
 }
 
 async function start() {
     const getData = await getInfo();
-    await createContent(getData);
-    await parseTrailer(getData);
+    const videoId = await parseTrailer(getData, API_KEY);
+    await createContent(getData, videoId);
 }
 
 start()
